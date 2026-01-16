daef9a99661def3965f684eb7ad1c6ddc05f61c7
haroonrashidzadran <haroonrashidzadran@gmail.com>
2026-01-16 14:48:40 +0430

    Add REST API endpoints

---
+++ backend/todoproject/tasks/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Task
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'priority', 'completed', 'due_date', 'created_at']
+    list_filter = ['completed', 'priority', 'created_at']
+    search_fields = ['title', 'description']
